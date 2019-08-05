1d4fad7c8ac4e81c9f49ec07fe8c4be951564caa
nicky <nicky@wizmacau.com>
2019-08-05 20:30:18 +0800

    update config

--- bash/init-function.bash
+++ bash/init-function.bash
@@ -13,7 +13,7 @@ source $SHARE_SHELL_PATH/autocomplete-symfony2.bash
 source $SHARE_SHELL_PATH/autocomplete-lunchy.bash
 
 function mcd(){
-	cd $USER_HOME/Web/www/$1.$SELF_DOMAIN/$1
+	cd $USER_HOME/Web/www/$1.$DOMAIN_SELF/$1
 }
 
 function init-sf1(){
@@ -38,8 +38,8 @@ function init-sf2(){
 			chmod +a "`whoami` allow delete,write,append,file_inherit,directory_inherit" app/cache app/logs web/uploads
 			chmod 777 app/cache app/logs web/uploads
 		else
-			setfacl -R -m u:apache:rwX -m u:`whoami`:rwX app/cache app/logs web/uploads
-			setfacl -dR -m u:apache:rwx -m u:`whoami`:rwx app/cache app/logs web/uploads
+			setfacl -R -m u:www:rwX -m u:`whoami`:rwX app/cache app/logs web/uploads
+			setfacl -dR -m u:www:rwx -m u:`whoami`:rwx app/cache app/logs web/uploads
 		fi
 		app/console assets:install --symlink
 		app/console assetic:dump
@@ -51,13 +51,34 @@ function init-sf2(){
 	fi
 }
 
+function init-sf3(){
+	if [[ -d "./vendor" ]]; then
+		mkdir var/cache var/logs var/sessions web/uploads
+		if [ "M" = "$SYS_TYPE" ]; then
+			chmod +a "_www allow delete,write,append,file_inherit,directory_inherit" var/cache var/logs var/sessions web/uploads
+			chmod +a "`whoami` allow delete,write,append,file_inherit,directory_inherit" var/cache var/logs var/sessions web/uploads
+			chmod 777 var/cache var/logs var/sessions web/uploads
+		else
+			setfacl -R -m u:www:rwX -m u:`whoami`:rwX var/cache var/logs web/uploads
+			setfacl -dR -m u:www:rwx -m u:`whoami`:rwx var/cache var/logs web/uploads
+		fi
+		bin/console assets:install --symlink --relative
+		bin/console assetic:dump
+		
+		bin/console cache:clear --env=dev --no-optional-warmers
+		bin/console cache:clear --env=prod --no-optional-warmers
+	else
+		echo "Please run in symfony project root path after composer update.";
+	fi
+}
+
 function sf2chmod(){
   if [ "M" = "$SYS_TYPE" ]; then
     chmod +a "_www allow delete,write,append,file_inherit,directory_inherit" $@
     chmod +a "`whoami` allow delete,write,append,file_inherit,directory_inherit" $@
     chmod 777 $@
   else
-    setfacl -R -m u:apache:rwX -m u:`whoami`:rwX $@
-    setfacl -dR -m u:apache:rwx -m u:`whoami`:rwx $@
+    setfacl -R -m u:www:rwX -m u:`whoami`:rwX $@
+    setfacl -dR -m u:www:rwx -m u:`whoami`:rwx $@
   fi
 }
